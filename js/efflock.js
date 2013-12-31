3b082e579dee9d8f3c3dda667c2c3dd29dea75d0
Andrew Leavitt <andrewleavitt@Andrews-Mac-Pro.local>
2013-12-31 15:20:51 -0700

    this wraps up the functional part

--- js/efflock.js
+++ js/efflock.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('#slabs a').click(function() {
+		efflock_amounts = new Array();
 		console.log('slabs clicked');
 		thickness = parseInt($('#thickness').val());
 		length = parseInt($('#length').val());
@@ -9,19 +10,43 @@ $(document).ready(function() {
 		console.log(cubic_yards);
 		efflock_amounts = getEfflock(cubic_yards);
 		console.log(efflock_amounts);
-		$('#slab-cubic-yards').html(cubic_yards);
-		$('#slab-efflock-recommended').html(cubic_yards);
+		$('#slabs-cubic-yards').html(cubic_yards);
+		$('#slabs-efflock-recommended_6_0').html(efflock_amounts[0]);
+		$('#slabs-efflock-recommended_6_5').html(efflock_amounts[1]);
+		$('#slabs-efflock-recommended_7_0').html(efflock_amounts[2]);
 	});
 
 	$('#footings a').click(function() {
+		efflock_amounts = new Array();
 		console.log('depth clicked');
 		depth = parseInt($('#footings #depth').val());
 		length = parseInt($('#footings #length').val());
 		width = parseInt($('#footings #width').val());
 		console.log(depth + ':' + length + ':' + width);
-		result = calculateCubicYards(length*12,width,depth);
-		console.log(result);
-		$('#footing-cubic-yards').html(result);
+		cubic_yards = calculateCubicYards(length,width,depth);
+		console.log(cubic_yards);
+		efflock_amounts = getEfflock(cubic_yards);
+		console.log(efflock_amounts);
+		$('#footings-cubic-yards').html(cubic_yards);
+		$('#footings-efflock-recommended_6_0').html(efflock_amounts[0]);
+		$('#footings-efflock-recommended_6_5').html(efflock_amounts[1]);
+		$('#footings-efflock-recommended_7_0').html(efflock_amounts[2]);
+	});
+
+	$('#columns a').click(function() {
+		efflock_amounts = new Array();
+		console.log('column clicked');
+		diameter = parseInt($('#columns #diameter').val());
+		height = parseInt($('#columns #height').val());
+		console.log(depth + ':' + length + ':' + width);
+		cubic_yards = calculateColumnarCubicYards(diameter,height);
+		console.log(cubic_yards);
+		efflock_amounts = getEfflock(cubic_yards);
+		console.log(efflock_amounts);
+		$('#columns-cubic-yards').html(cubic_yards);
+		$('#columns-efflock-recommended_6_0').html(efflock_amounts[0]);
+		$('#columns-efflock-recommended_6_5').html(efflock_amounts[1]);
+		$('#columns-efflock-recommended_7_0').html(efflock_amounts[2]);
 	});
 });
 
@@ -35,7 +60,7 @@ var calculateCubicYards = function(length, width, thickness) {
 
 // takes inches
 var calculateColumnarCubicYards = function(diameter, height) {
-	return (Math.pow(diameter/2, 2)*Math.PI*height) / 27;
+	return ((Math.pow(diameter/2, 2)*Math.PI*height) / 27).toFixed(2);
 }
 
 var getEfflock = function(cubic_yards) {
@@ -43,7 +68,7 @@ var getEfflock = function(cubic_yards) {
 	var efflock_amounts = new Array();
 
     for(i=0; i<cement_weights.length; i++) {
-		efflock_amounts[cement_weights[i].toString()] = calculateEfflock(cubic_yards,cement_weights[i]);     
+		efflock_amounts.push(calculateEfflock(cubic_yards,cement_weights[i]));
 	}
 	return efflock_amounts;
 }
@@ -53,12 +78,12 @@ var calculateEfflock = function(cubic_yards, cement_weight) {
 	var efflock = 0;
 	if(cement_weight == '6.0') {
 		efflock = cubic_yards * 45.12;
-	} else if (cement_weight = '6.5') {
+	} else if (cement_weight == '6.5') {
 		efflock = cubic_yards * 48.88;
-	} else if (cement_weight = '7.0') {
+	} else if (cement_weight == '7.0') {
 		efflock = cubic_yards * 52.64;
 	}
 
-	return efflock;
+	return efflock.toFixed(2);
 
 }
